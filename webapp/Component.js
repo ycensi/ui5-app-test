5984e2dcfb180e0d1bc2daf28e0a7132d9da1335
Yuri Robert Censi <yuri.censi@roge.local>
2017-03-07 17:43:54 -0300

    Até o passo 15 do tutorial

---
+++ webapp/Component.js
@@ -0,0 +1,30 @@
+sap.ui.define([
+   "sap/ui/core/UIComponent",
+   "sap/ui/model/json/JSONModel",
+   "sap/ui/model/resource/ResourceModel"
+], function (UIComponent, JSONModel, ResourceModel) {
+   "use strict";
+   return UIComponent.extend("app.Component", {
+    metadata : {
+            manifest: "json"
+      },
+      init : function () {
+         // call the init function of the parent
+         UIComponent.prototype.init.apply(this, arguments);
+         // set data model
+         var oData = {
+            recipient : {
+               name : "World"
+            }
+         };
+         var oModel = new JSONModel(oData);
+         this.setModel(oModel);
+
+         // set i18n model
+         var i18nModel = new ResourceModel({
+            bundleName : "app.i18n.i18n"
+         });
+         this.setModel(i18nModel, "i18n");
+      }
+   });
+});
